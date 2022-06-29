ca61ad4c9bcc4172064c2b9940ab73027af916fd
Atrya537 <101434393+Atrya537@users.noreply.github.com>
2022-06-29 12:44:57 -0400

    Tested different items on newegg

--- price_checker.py
+++ price_checker.py
@@ -1,40 +1,40 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.newegg.com/evga-geforce-rtx-3090-24g-p5-3987-kr/p/N82E16814487526?Item=N82E16814487526&Description=rtx%203090&cm_re=rtx_3090-_-14-487-526-_-Product&quicklink=true'
+url = 'https://www.newegg.com/huanuo-hnssk4-black/p/15Z-01XB-00049?Item=9SIAJDS9C72608&cm_sp=Homepage_dailydeals-_-P1_9SIAJDS9C72608-_-06292022&quicklink=true'
 
 result = requests.get(url)
 doc = BeautifulSoup(result.text, 'html.parser')
 
 def get_current_price():
     prices = doc.find(text='$').parent
-    gpuPrice = prices.find('strong').text
-    return gpuPrice
+    itemPrice = prices.find('strong').text
+    return itemPrice
 
 def current_price_to_int():
-    currentPriceToInt = []
+    intCurrentPrice = []
 
     currentPriceAsArray = get_current_price().split(',')
     for i in currentPriceAsArray:
         if (',' not in i):
-            currentPriceToInt.append(i)
+            intCurrentPrice.append(i)
 
-    return (int(''.join(currentPriceToInt)))
+    return (int(''.join(intCurrentPrice)))
 
 def get_original_price():
     priceWas = doc.find_all('span', {'class': 'price-was-data'})[0]
     return(priceWas.text)
 
 def original_price_to_int():
-    intPrice = []
+    intOriginalPrice = []
     
     originalPriceAsArray = list(get_original_price())
     for i in originalPriceAsArray:
         if (i.isdigit()):
-            intPrice.append(i)
+            intOriginalPrice.append(i)
 
-    del intPrice[-2:]
-    return int(''.join(intPrice))
+    del intOriginalPrice[-2:]
+    return int(''.join(intOriginalPrice))
 
-print(original_price_to_int())
-print(current_price_to_int())
+if (original_price_to_int() > current_price_to_int()):
+    print(f'Your item is ${original_price_to_int() - current_price_to_int()} off!')
